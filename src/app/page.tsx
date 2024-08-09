e23d1d6a7fd268699be8bf50567febc8813a5b58
Nirclaw <caicedonicolas150@gmail.com>
2024-08-09 17:48:03 -0500

    feat: Actualizacion de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import Styles from "@/app/Socialmedia.module.css";
 import Carsds from "@/components/cards/Carsds";
+import { useSession } from "next-auth/react";
 import React, { useState } from 'react';
 
-// Definición de la estructura del personaje
 interface Character {
   id: number;
   name: string;
@@ -24,10 +24,14 @@ const SocialMedia: React.FC = () => {
     setCharacters(info);
   };
 
+  const { data: sesion } = useSession()
+
 
   return (
+
+
     <div>
-      <div className={Styles.socialmedia}>
+      {sesion?.user ? (<div className={Styles.socialmedia}>
         <div className={Styles.estado}>Estados</div>
         <div className={Styles.contenedor}>
           <button className={Styles.oauthButton} value="Alive" onClick={mostrar}>Alive</button>
@@ -47,7 +51,8 @@ const SocialMedia: React.FC = () => {
             />
           ))}
         </div>
-      </div>
+      </div>) : ""}
+
     </div>
   );
 };
